c8eb87d4b2894f798e1e351e778a4d464256c417
erickmorais-taskup <mendes.morais.erick@gmail.com>
2026-01-18 04:36:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabase = createClient(
-  "https://SEU-PROJETO.supabase.co",
-  "SUA_ANON_KEY"
+  "https://bfynkxmdsydbmkdttdok.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmeW5reG1kc3lkYm1rZHR0ZG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MTQ3NzEsImV4cCI6MjA4NDI5MDc3MX0.Dvbijztg4bHPcxgjVhpfGcAfwNJrbv2CsuGktG9nqyg"
 );
 
 async function mostrarServico(servico) {
