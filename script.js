5e005a705120b85ca46ed8c29812f097ec106e3b
erickmorais-taskup <mendes.morais.erick@gmail.com>
2026-01-18 04:59:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // ===============================
-// SUPABASE CLIENT (ANTI-DUPLICAÇÃO)
+// SUPABASE CLIENT (GLOBAL SEGURO)
 // ===============================
-var supabase = window.supabaseClient;
+var supabase;
 
-if (!supabase) {
-    window.supabaseClient = window.supabase.createClient(
-        "https://bfynkxmdsydbmkdttdok.supabase.co",
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmeW5reG1kc3lkYm1rZHR0ZG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MTQ3NzEsImV4cCI6MjA4NDI5MDc3MX0.Dvbijztg4bHPcxgjVhpfGcAfwNJrbv2CsuGktG9nqyg"
-    );
+document.addEventListener("DOMContentLoaded", () => {
+    if (!window.supabase) {
+        console.error("Supabase SDK não carregou");
+        return;
+    }
+
+    if (!window.supabaseClient) {
+        window.supabaseClient = window.supabase.createClient(
+            "https://bfynkxmdsydbmkdttdok.supabase.co",
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmeW5reG1kc3lkYm1rZHR0ZG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MTQ3NzEsImV4cCI6MjA4NDI5MDc3MX0.Dvbijztg4bHPcxgjVhpfGcAfwNJrbv2CsuGktG9nqyg"
+        );
+    }
+
     supabase = window.supabaseClient;
-}
+});
 
 async function mostrarServico(servico) {
     const { data, error } = await supabase
