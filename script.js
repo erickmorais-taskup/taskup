52d8411af4d94b6cfaa6d32154365ec8233bf76e
erickmorais-taskup <mendes.morais.erick@gmail.com>
2026-01-20 01:22:06 -0300

    Att

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-let supabase = null;
+console.log("script.js carregado");
 
-document.addEventListener("DOMContentLoaded", async () => {
-  if (!window.supabase) {
-    console.error("SDK do Supabase não carregou");
-    return;
-  }
+const SUPABASE_URL = "https://bfynkxmdsydbmkdttdok.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmeW5reG1kc3lkYm1rZHR0ZG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MTQ3NzEsImV4cCI6MjA4NDI5MDc3MX0.Dvbijztg4bHPcxgjVhpfGcAfwNJrbv2CsuGktG9nqyg";
 
-  supabase = window.supabase.createClient(
-    "https://bfynkxmdsydbmkdttdok.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmeW5reG1kc3lkYm1rZHR0ZG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3MTQ3NzEsImV4cCI6MjA4NDI5MDc3MX0.Dvbijztg4bHPcxgjVhpfGcAfwNJrbv2CsuGktG9nqyg"
-  );
-
-  console.log("Supabase pronto");
-
-  await mostrarUsuarioLogado();
-});
+if (!window.supabase) {
+    console.error("Supabase SDK NÃO carregou");
+} else {
+    window.supabaseClient = window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_ANON_KEY
+    );
+    console.log("Supabase client criado:", window.supabaseClient);
+}
 
 // NOME APARECE NA HEADER
 async function mostrarUsuarioLogado() {
